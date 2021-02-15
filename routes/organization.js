2dff267c7a22eae6f16b7c5cfa3374077b5e977c
aditya hadiwijaya <aditya.hadiwijaya@jatis.com>
2021-02-15 16:25:28 +0700

    update route

--- routes/organization.js
+++ routes/organization.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const { body } = require('express-validator');
 const organizationController = require('../controllers/organization');
 
+router.get('/list',  [
+], organizationController.list);
+
 router.post('/create',  [
     body('name').trim().isLength({ min: 5 }),
     body('desc').trim().isLength({ min: 5 }),
 ], organizationController.create);
 
-router.post('/signin',  [
-    body('email').trim().isLength({ min: 5 }),
-    body('password').trim().isLength({ min: 8 }),
-], organizationController.signin);
+router.post('/edit',  [
+    body('name').trim().isLength({ min: 5 }),
+    body('desc').trim().isLength({ min: 8 }),
+], organizationController.edit);
+
+router.post('/delete',  [
+    body('id').trim().isLength({ min: 5 }),
+], organizationController.delete);
 
 module.exports = router;
