98ece5633a05c119d1b303fa8872235514e1acef
aditya hadiwijaya <aditya.hadiwijaya@jatis.com>
2021-02-16 11:35:59 +0700

    add poi

--- controllers/poi.js
+++ controllers/poi.js
@@ -4,127 +4,116 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const utils = require('../utils/utils');
 const config = require('../config/auth.config');
-const db = require('../models');
-const User = db.user;
-const Organization = db.organization;
+const User = require("../models/user");
+const Poi = require("../models/poi");
 
-exports.getAllOrganizations = async (req, res, next) => {
-    utils.authenticateJWT(req, res, next);
-    Organization.find()
-        .then((organizations) => {
-            res.status(200).json({
-                error: 0,
-                message: "Fetch data organization success",
-                data: organizations,
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                message: err,
-            });
-        });
+exports.getAll = async (req, res, next) => {
+  utils.authenticateJWT(req, res, next);
+  Organization.find()
+    .then((organizations) => {
+      res.status(200).json({
+        error: 0,
+        message: "Fetch data poi success",
+        data: organizations,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err,
+      });
+    });
 };
 
 exports.edit = async (req, res, next) => {
-    Organization
-        .findByIdAndUpdate(
-            req.body.id,
-            { $set: { name: req.body.name, desc: req.body.desc } },
-            { new: true },
-            (err, doc) => {
-                if (err) {
-                    res.status(500).send({ message: err });
-                    return;
-                }
-                res.send({
-                    error: 0,
-                    message: "Organization was edited successfully!"
-                });
-            });
+  utils.authenticateJWT(req, res, next);
+
+  let user = await User.findOne({
+    _id: ObjectId(req.user.id)
+  })
+
+  Organization
+    .findByIdAndUpdate(
+      req.body.id,
+      {
+        $set: {
+          name: req.body.name,
+          desc: req.body.desc,
+          siteId: req.body.siteId,
+          category: req.body.category,
+          floor: req.body.floor,
+          location: req.body.location,
+          logo: req.body.logo,
+          logoUrl: req.body.logoUrl,
+          opentime: req.body.opentime,
+          phone: req.body.phone,
+          priority: req.body.priority,
+          tag: req.body.tag,
+          url: req.body.url,
+          modifiedAt: new Date(),
+          modifiedBy: user.email,
+        }
+      },
+      { new: true },
+      (err, doc) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        res.send({
+          error: 0,
+          message: "Poi was edited successfully!"
+        });
+      });
 }
 
 exports.delete = async (req, res, next) => {
-    Organization.findByIdAndRemove(
-        req.body.id,
-        (err, doc) => {
-            if (err) {
-                res.status(500).send({ message: err });
-                return;
-            }
-            res.send({
-                error: 0,
-                message: "Organization was deleted successfully!"
-            });
-        });
+  Organization.findByIdAndRemove(
+    req.body.id,
+    (err, doc) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      res.send({
+        error: 0,
+        message: "Poi was deleted successfully!"
+      });
+    });
 }
 
 exports.create = async (req, res, next) => {
-    utils.authenticateJWT(req, res, next);
+  utils.authenticateJWT(req, res, next);
 
-    let user = await User.findOne({
-        _id: ObjectId(req.user.id)
-    })
+  let user = await User.findOne({
+    _id: ObjectId(req.user.id)
+  })
 
-    const organization = new Organization({
-        name: req.body.name,
-        desc: req.body.desc,
-        modifiedAt: new Date(),
-        modifiedBy: user.email,
+  const poi = new Poi({
+    name: req.body.name,
+    desc: req.body.desc,
+    siteId: req.body.siteId,
+    category: req.body.category,
+    floor: req.body.floor,
+    location: req.body.location,
+    logo: req.body.logo,
+    logoUrl: req.body.logoUrl,
+    opentime: req.body.opentime,
+    phone: req.body.phone,
+    priority: req.body.priority,
+    tag: req.body.tag,
+    url: req.body.url,
+    modifiedAt: new Date(),
+    modifiedBy: user.email,
+  });
+
+  Poi.save((err, org) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.send({
+      error: 0,
+      message: "Poi was added successfully!"
     });
-
-    organization.save((err, org) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
-        res.send({
-            error: 0,
-            message: "Organization was added successfully!"
-        });
-    });
+  });
 };
-
-exports.signin = (req, res) => {
-    User.findOne({
-        email: req.body.email
-    })
-        .exec((err, user) => {
-            if (err) {
-                res.status(500).send({ message: err });
-                return
-            }
-
-            if (!user) {
-                return res.status(404).send({ message: "User Not found." });
-            }
-
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-
-            if (!passwordIsValid) {
-                return res.status(401).send({
-                    error: 1,
-                    message: "Invalid Password!"
-                });
-            }
-
-            const token = jwt.sign({ id: user.id }, config.secret, {
-                expiresIn: 86400 // 24 hours
-            });
-
-            var authorities = [];
-
-            // for (let i = 0; i < user.roles.length; i++) {
-            //     authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-            // }
-            res.status(200).send({
-                error: 0,
-                id: user._id,
-                email: user.email,
-                name: user.name,
-                accessToken: token
-            });
-        });
-}
--- routes/poi.js
+++ routes/poi.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const organizationController = require('../controllers/organization');
+const poiController = require('../controllers/poi');
 
 router.get('/list',  [
-], organizationController.getAllOrganizations);
+], poiController.getAll);
 
 router.post('/create',  [
     body('name').trim().isLength({ min: 5 }),
-    body('desc').trim().isLength({ min: 5 }),
-], organizationController.create);
+    body('desc').trim(),
+    body('category').trim(),
+    body('floor').trim(),
+    body('location').trim(),
+    body('logo').trim(),
+    body('logoUrl').trim(),
+    body('opentime').trim(),
+    body('phone').trim(),
+    body('priority').trim(),
+    body('siteId').trim(),
+    body('tag').trim(),
+    body('url').trim(),
+], poiController.create);
 
 router.put('/edit',  [
     body('id').trim().isLength({ min: 5 }),
     body('name').trim().isLength({ min: 5 }),
     body('desc').trim().isLength({ min: 8 }),
-], organizationController.edit);
+], poiController.edit);
 
 router.delete('/delete',  [
     body('id').trim().isLength({ min: 5 }),
-], organizationController.delete);
+], poiController.delete);
 
 module.exports = router;
