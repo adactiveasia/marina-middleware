4efcf781e996489d78a62bc9388f0421a711f7e3
aditya hadiwijaya <aditya.hadiwijaya@jatis.com>
2021-02-16 10:15:34 +0700

    init create poi and poi category

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
 // const siteRoutes = require('./routes/site');
 const organizationRoutes = require('./routes/organization');
+const poiCategoryRoutes = require('./routes/poiCategory');
 
 const dbConfig = require('./config/db.config');
 
@@ -57,6 +58,7 @@ app.use('/user', userRoutes);
 app.use('/auth', authRoutes);
 // app.use('/site', siteRoutes);
 app.use('/org', organizationRoutes);
+app.use('/poiCategory', poiCategoryRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
---
+++ routes/poi.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator');
+const organizationController = require('../controllers/organization');
+
+router.get('/list',  [
+], organizationController.getAllOrganizations);
+
+router.post('/create',  [
+    body('name').trim().isLength({ min: 5 }),
+    body('desc').trim().isLength({ min: 5 }),
+], organizationController.create);
+
+router.put('/edit',  [
+    body('id').trim().isLength({ min: 5 }),
+    body('name').trim().isLength({ min: 5 }),
+    body('desc').trim().isLength({ min: 8 }),
+], organizationController.edit);
+
+router.delete('/delete',  [
+    body('id').trim().isLength({ min: 5 }),
+], organizationController.delete);
+
+module.exports = router;
