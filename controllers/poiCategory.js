283120e51bb0231e2c060adc1fe81ccc0d6560d7
aditya hadiwijaya <aditya.hadiwijaya@jatis.com>
2021-02-16 10:31:20 +0700

    check user auth and add edited by info

--- controllers/poiCategory.js
+++ controllers/poiCategory.js
@@ -5,12 +5,12 @@ const bcrypt = require('bcryptjs');
 const utils = require('../utils/utils');
 const config = require('../config/auth.config');
 const db = require('../models');
-const User = db.user;
-const PoiCategory = db.poiCategory;
+const User = require("../models/user");
+const PoiCategory = require("../models/poiCategory");
 
 exports.getAll = async (req, res, next) => {
     utils.authenticateJWT(req, res, next);
-    
+
     PoiCategory.find()
         .then((poiCategory) => {
             res.status(200).json({
@@ -27,10 +27,23 @@ exports.getAll = async (req, res, next) => {
 };
 
 exports.edit = async (req, res, next) => {
-    Organization
+    utils.authenticateJWT(req, res, next);
+
+    let user = await User.findOne({
+        _id: ObjectId(req.user.id)
+    })
+
+    PoiCategory
         .findByIdAndUpdate(
             req.body.id,
-            { $set: { name: req.body.name, desc: req.body.desc } },
+            {
+                $set: {
+                    name: req.body.name,
+                    desc: req.body.desc,
+                    modifiedAt: new Date(),
+                    modifiedBy: user.email,
+                }
+            },
             { new: true },
             (err, doc) => {
                 if (err) {
@@ -39,13 +52,15 @@ exports.edit = async (req, res, next) => {
                 }
                 res.send({
                     error: 0,
-                    message: "Organization was edited successfully!"
+                    message: "Poi Category was edited successfully!"
                 });
             });
 }
 
 exports.delete = async (req, res, next) => {
-    Organization.findByIdAndRemove(
+    utils.authenticateJWT(req, res, next);
+
+    PoiCategory.findByIdAndRemove(
         req.body.id,
         (err, doc) => {
             if (err) {
@@ -54,7 +69,7 @@ exports.delete = async (req, res, next) => {
             }
             res.send({
                 error: 0,
-                message: "Organization was deleted successfully!"
+                message: "Poi Category was deleted successfully!"
             });
         });
 }
@@ -85,48 +100,3 @@ exports.create = async (req, res, next) => {
         });
     });
 };
-
-exports.signin = (req, res) => {
-    User.findOne({
-        email: req.body.email
-    })
-        .exec((err, user) => {
-            if (err) {
-                res.status(500).send({ message: err });
-                return
-            }
-
-            if (!user) {
-                return res.status(404).send({ message: "User Not found." });
-            }
-
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-
-            if (!passwordIsValid) {
-                return res.status(401).send({
-                    error: 1,
-                    message: "Invalid Password!"
-                });
-            }
-
-            const token = jwt.sign({ id: user.id }, config.secret, {
-                expiresIn: 86400 // 24 hours
-            });
-
-            var authorities = [];
-
-            // for (let i = 0; i < user.roles.length; i++) {
-            //     authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-            // }
-            res.status(200).send({
-                error: 0,
-                id: user._id,
-                email: user.email,
-                name: user.name,
-                accessToken: token
-            });
-        });
-}
